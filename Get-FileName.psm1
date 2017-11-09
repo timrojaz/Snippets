88eb286b1c83763b039ab3d104e3de3cbaf09d0f
timrojaz <33521896+timrojaz@users.noreply.github.com>
2017-11-09 15:25:07 +0000

    Create Get-FileName.psm1

---
+++ Get-FileName.psm1
@@ -0,0 +1,14 @@
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+$file = Get-FileName "%UserProfile%\Desktop"
+$inputdata = get-content $file
+$inputdata
